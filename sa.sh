625d3dcc408c195c72fbf49fb3f493a792878c65
Toby Woller <tobywoller@gmail.com>
2014-07-29 14:46:05 -0700

    3.6.2 Automated tests with Guard
    One annoyance associated with using the rspec command is having to
    switch to the command line and run the tests by hand. (A second
    annoyance, the slow start-up time of the test suite, is addressed in
    Section 3.6.3.) In this section, we’ll show how to use Guard to automate
    the running of the tests. Guard monitors changes in the filesystem so
    that, for example, when we change the static_pages_spec.rb file only
    those tests get run. Even better, we can configure Guard so that when,
    say, the home.html.erb file is modified, the static_pages_spec.rb
    automatically runs.

--- sa.sh
+++ sa.sh
@@ -4,23 +4,23 @@ cd  ~/rails_projects/sample_app/
    
 tmux start-server
 tmux new-session -d -s sa -n root
-tmux new-window -t sa:2 -n console
-tmux new-window -t sa:3 -n config
-tmux new-window -t sa:4 -n views
-tmux new-window -t sa:5 -n controllers
-tmux new-window -t sa:6 -n spec
-tmux new-window -t sa:7 -n webrick 
-tmux new-window -t sa:8 -n git
-tmux new-window -t sa:9 -n log
+tmux new-window -t sa:2 -n config
+tmux new-window -t sa:3 -n views
+tmux new-window -t sa:4 -n controllers
+tmux new-window -t sa:5 -n spec
+tmux new-window -t sa:6 -n guard
+tmux new-window -t sa:7 -n log
+tmux new-window -t sa:8 -n webrick 
+tmux new-window -t sa:9 -n git
    
-tmux send-keys -t sa:2 "rails c --sandbox" C-m
-tmux send-keys -t sa:3 "cd config/; ls -la" C-m
-tmux send-keys -t sa:4 "cd app/views/; ls -la" C-m
-tmux send-keys -t sa:5 "cd app/controllers/; ls -la" C-m
-tmux send-keys -t sa:6 "cd spec/; ls -la" C-m
-tmux send-keys -t sa:7 "rails s" C-m
-tmux send-keys -t sa:8 "git status" C-m
-tmux send-keys -t sa:9 "cd log/" C-m
+tmux send-keys -t sa:2 "cd config/; ls -la" C-m
+tmux send-keys -t sa:3 "cd app/views/; ls -la" C-m
+tmux send-keys -t sa:4 "cd app/controllers/; ls -la" C-m
+tmux send-keys -t sa:5 "cd spec/; ls -la" C-m
+tmux send-keys -t sa:6 "guard" C-m
+tmux send-keys -t sa:7 "cd log/; ls -la" C-m
+tmux send-keys -t sa:8 "rails s" C-m
+tmux send-keys -t sa:9 "git status" C-m
    
-tmux select-window -t sa:8
+tmux select-window -t sa:9
 tmux attach-session -t sa
