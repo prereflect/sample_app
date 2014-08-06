a44f87db2283457c19030f46d6310f86d938f5ae
Toby Woller <tobywoller@gmail.com>
2014-08-06 08:48:39 -0700

    11.2 A web interface for following users
    In the introduction to this chapter, we saw a preview of the page flow
    for user following. In this section, we will implement the basic
    interface and following/unfollowing functionality shown in those
    mockups. We will also make separate pages to show the user following and
    followers arrays. In Section 11.3, we’ll complete our sample application
    by adding the user’s status feed.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  
   subject { page }
  
   shared_examples_for "all static pages" do
+
     it { should have_selector('h1', text: heading) }
     it { should have_title(full_title(page_title)) }
   end
@@ -13,14 +13,15 @@ describe "Static pages" do
     before { visit root_path }
     let(:heading)    { 'Sample App' }
     let(:page_title) { '' }
+
     it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
 
     describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        FactoryGirl.create(:micropost, user: user, content: "Lorem")
+        FactoryGirl.create(:micropost, user: user, content: "Ipsum")
         sign_in user
         visit root_path
       end
@@ -30,6 +31,17 @@ describe "Static pages" do
           expect(page).to have_selector("li##{item.id}", text: item.content)
         end
       end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
     end
   end
 
@@ -37,6 +49,7 @@ describe "Static pages" do
     before { visit help_path }
     let(:heading)    { 'Help' }
     let(:page_title) { 'Help' }
+
     it_should_behave_like "all static pages"
   end
 
@@ -44,6 +57,7 @@ describe "Static pages" do
     before { visit about_path }
     let(:heading)    { 'About' }
     let(:page_title) { 'About' }
+
     it_should_behave_like "all static pages"
   end
 
@@ -51,6 +65,7 @@ describe "Static pages" do
     before { visit contact_path }
     let(:heading)    { 'Contact' }
     let(:page_title) { 'Contact' }
+
     it_should_behave_like "all static pages"
   end
 
@@ -68,5 +83,4 @@ describe "Static pages" do
     click_link "sample app"
     expect(page).to have_title(full_title(''))
   end
-
 end
